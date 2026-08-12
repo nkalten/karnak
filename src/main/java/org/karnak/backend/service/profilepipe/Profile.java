3fc1c4f0b4bb9e3377ee1bd3a59ac11f49e49e0e
Noah K <shoutizix@gmail.com>
2026-08-12 14:20:08 +0200

    chore: format profile imports

--- src/main/java/org/karnak/backend/service/profilepipe/Profile.java
+++ src/main/java/org/karnak/backend/service/profilepipe/Profile.java
@@ -9,9 +9,6 @@
  */
 package org.karnak.backend.service.profilepipe;
 
-import static org.karnak.backend.dicom.DefacingUtil.isAxial;
-import static org.karnak.backend.dicom.DefacingUtil.isCT;
-
 import java.awt.*;
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -22,6 +19,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+
 import lombok.extern.slf4j.Slf4j;
 import org.dcm4che3.data.Attributes;
 import org.dcm4che3.data.BulkData;
@@ -60,6 +58,9 @@ import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 import org.weasis.core.util.StringUtil;
 import org.weasis.dicom.param.AttributeEditorContext;
+
+import static org.karnak.backend.dicom.DefacingUtil.isAxial;
+import static org.karnak.backend.dicom.DefacingUtil.isCT;
 
 @Slf4j
 public class Profile {
